2a675fc24c4413f2b93541427f762d6047b3590c
sugnakys <sugnakys@gmail.com>
2017-03-08 22:28:36 +0900

    Add Permission check

--- UsbSerialConsole/app/src/main/java/jp/sugnakys/usbserialconsole/settings/LogPreferenceFragment.java
+++ UsbSerialConsole/app/src/main/java/jp/sugnakys/usbserialconsole/settings/LogPreferenceFragment.java
@@ -1,11 +1,21 @@
 package jp.sugnakys.usbserialconsole.settings;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceManager;
 import android.preference.SwitchPreference;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 
 import net.rdrei.android.dirchooser.DirectoryChooserConfig;
@@ -15,9 +25,11 @@ import jp.sugnakys.usbserialconsole.R;
 import jp.sugnakys.usbserialconsole.util.Log;
 
 public class LogPreferenceFragment extends BasePreferenceFragment
-        implements Preference.OnPreferenceClickListener, DirectoryChooserFragment.OnFragmentInteractionListener {
+        implements Preference.OnPreferenceClickListener, DirectoryChooserFragment.OnFragmentInteractionListener, PermissionResultListener {
 
     private static final String TAG = "LogPreferenceFragment";
+
+    private final int REQUEST_CODE_WRITE_STORAGE_PERMISSION = 1;
 
     private SwitchPreference switchStoragePreference;
     private Preference saveLocationPreference;
@@ -37,7 +49,7 @@ public class LogPreferenceFragment extends BasePreferenceFragment
         }
 
         switchStoragePreference = (SwitchPreference) findPreference(getString(R.string.log_switch_storage_key));
-        saveLocationPreference = (Preference) findPreference(getString(R.string.log_directory_key));
+        saveLocationPreference = findPreference(getString(R.string.log_directory_key));
     }
 
     @Override
@@ -47,9 +59,20 @@ public class LogPreferenceFragment extends BasePreferenceFragment
         Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
         toolbar.setTitle(getString(R.string.log_title));
 
+        checkStoragePermission();
+
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getActivity());
         saveLocationPreference.setSummary(pref.getString(getString(R.string.log_directory_key), android.os.Environment.getExternalStorageDirectory().getAbsolutePath()));
         setSaveLocationEnable(switchStoragePreference.isChecked());
+    }
+
+    private void checkStoragePermission() {
+        int permission = PermissionChecker.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            switchStoragePreference.setChecked(true);
+        } else {
+            switchStoragePreference.setChecked(false);
+        }
     }
 
     @Override
@@ -57,6 +80,13 @@ public class LogPreferenceFragment extends BasePreferenceFragment
         String key = preference.getKey();
 
         if (key.equals(getString(R.string.log_switch_storage_key))) {
+            if (switchStoragePreference.isChecked()) {
+                int permissionCheck = PermissionChecker.checkSelfPermission(getActivity(),
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                    requestStoragePermission();
+                }
+            }
         } else if (key.equals(getString(R.string.log_directory_key))) {
             Log.d(TAG, android.os.Environment.getExternalStorageDirectory().getAbsolutePath());
             final DirectoryChooserConfig config = DirectoryChooserConfig.builder()
@@ -71,6 +101,69 @@ public class LogPreferenceFragment extends BasePreferenceFragment
         }
 
         return false;
+    }
+
+    private void requestStoragePermission() {
+        Log.d(TAG, "requestStoragePermission");
+
+        Activity activity = getActivity();
+        if (activity != null && activity instanceof SettingsActivity) {
+            SettingsActivity settingsActivity = (SettingsActivity) activity;
+            ActivityCompat.requestPermissions(settingsActivity,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_CODE_WRITE_STORAGE_PERMISSION);
+            settingsActivity.setPermissionResultListener(this);
+        }
+    }
+
+    public void onPermissionResult(int requestCode, String[] permissions, int[] grantResults) {
+        Log.d(TAG, "onPermissionResult");
+        switch (requestCode) {
+            case REQUEST_CODE_WRITE_STORAGE_PERMISSION:
+                for (int i = 0; i < permissions.length; i++) {
+                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                        Log.d(TAG, "onRequestPermissionsResult: GRANTED");
+                    } else {
+                        Log.d(TAG, "onRequestPermissionsResult: DENIED");
+                        switchStoragePreference.setChecked(false);
+
+                        boolean showRationale = ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), permissions[i]);
+                        if (!showRationale) {
+                            // User choose "Don't ask again"
+                            new AlertDialog.Builder(getActivity())
+                                    .setMessage(getString(R.string.permission_storage_message))
+                                    .setPositiveButton(android.R.string.ok, null)
+                                    .setNeutralButton(R.string.permission_storeage_open_settings,
+                                            new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialogInterface, int i) {
+                                                    openSettings();
+                                                }
+                                            })
+                                    .create()
+                                    .show();
+                        } else if (Manifest.permission.WRITE_EXTERNAL_STORAGE.equals(permissions[i])) {
+                            new AlertDialog.Builder(getActivity())
+                                    .setMessage(getString(R.string.permission_storage_message))
+                                    .setPositiveButton(android.R.string.ok, null)
+                                    .create()
+                                    .show();
+                        }
+                    }
+                }
+                break;
+            default:
+                Log.e(TAG, "Unknown request: " + requestCode);
+                break;
+        }
+    }
+
+    private void openSettings() {
+        Log.d(TAG, "openSettings");
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getActivity().getPackageName(), null);
+        intent.setData(uri);
+        startActivity(intent);
     }
 
     @Override
--- UsbSerialConsole/app/src/main/java/jp/sugnakys/usbserialconsole/settings/SettingsActivity.java
+++ UsbSerialConsole/app/src/main/java/jp/sugnakys/usbserialconsole/settings/SettingsActivity.java
@@ -2,13 +2,20 @@ package jp.sugnakys.usbserialconsole.settings;
 
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 
 import jp.sugnakys.usbserialconsole.BaseAppCompatActivity;
 import jp.sugnakys.usbserialconsole.R;
+import jp.sugnakys.usbserialconsole.util.Log;
 
 public class SettingsActivity extends BaseAppCompatActivity {
+
+    private static final String TAG = "SettingsActivity";
+
+    private PermissionResultListener mPermissionResultListener = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +37,19 @@ public class SettingsActivity extends BaseAppCompatActivity {
         if (actionBar != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    public void setPermissionResultListener(PermissionResultListener mPermissionResultListener) {
+        this.mPermissionResultListener = mPermissionResultListener;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult");
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (mPermissionResultListener != null) {
+            mPermissionResultListener.onPermissionResult(requestCode, permissions, grantResults);
         }
     }
 }
